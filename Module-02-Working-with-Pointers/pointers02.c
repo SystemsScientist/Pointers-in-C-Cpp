684163435102edda7c8e4748253b9d1cd2abf816
Matthew Johnson <matthewjohnson@snarkydata.com>
2022-11-21 05:59:00 -0600

    Adding Module 02 files

---
+++ Module-02-Working-with-Pointers/pointers02.c
@@ -0,0 +1,24 @@
+// pointers02.c, Module 01, Pointers in C/C++
+// program demonstrates variables and pointers
+//
+// compile and execute program with the following
+// command-line scripts:
+// $ gcc pointers02.c -o main
+// ./main
+
+#include <stdio.h>
+
+int main(void) {
+
+	int a;
+	int *p;
+
+	a = 10;
+	p = &a;
+
+	printf("%d \n", p);
+	printf("%d \n", *p); // *p - value at address pointed to by p
+	printf("%d \n", &a);
+
+	return 0;
+}
