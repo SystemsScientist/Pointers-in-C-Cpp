8ac08077473a06b62b94a3a8624584b48a1e07a7
Matthew Johnson <matthewjohnson@snarkydata.com>
2022-11-25 06:23:31 -0600

    Adding pointers11.c file

---
+++ Module-04-Pointers-to-Pointers/pointers11.c
@@ -0,0 +1,28 @@
+// pointers11.c, Module 04, Pointers in C/C++
+// program demonstrates variables and pointers
+//
+// compile and execute program with the following
+// command-line scripts:
+// $ gcc pointers11.c -o main
+// $ ./main
+
+#include <stdio.h>
+
+int main(void) {
+
+	int x = 5;
+	int *p = &x;
+	*p = 6;
+	
+	int **q = &p;
+	int ***r = &q;
+
+	printf("\n%d\n", *p);
+	printf("%d\n", *q);
+	printf("%d\n", *(*q));
+
+	printf("%d\n", *(*r));
+	printf("%d\n\n", *(*(*r)));
+
+	return 0;
+}
